5f64400a1e691ac263e822755fe378304d1b7795
marwan-hafez77 <marwan.2025000971@seu.edu.eg>
2026-09-07 19:24:54 +0300

    I solve The BUG in Optimizer

--- Main/main_V2.py
+++ Main/main_V2.py
@@ -1524,51 +1524,51 @@ with tab2:
                 "Study plan generated successfully!"
             )
             total_hours = min(
-    float(available_hours),
-    float(max_daily_hours),
-)
+                float(available_hours),
+                float(max_daily_hours),
+            )
 
          # ==================================
          # Optimize Self-Study Plan
          # ==================================
-from dataclass.optimizer import StudyOptimizer
+            from dataclass.optimizer import StudyOptimizer
 
-study_plan = StudyOptimizer.allocate_study_time(
-       available_hours=total_hours,
-       courses=course_list  
-       )
+            study_plan = StudyOptimizer.allocate_study_time(
+                available_hours=total_hours,
+                courses=course_list  
+            )
 
-if study_plan:
-        for course_name, details in study_plan.items():
+            if study_plan:
+                for course_name, details in study_plan.items():
 
-            difficulty = details["difficulty"]
-            recommended_time = details["allocated_hours"]
+                    difficulty = details["difficulty"]
+                    recommended_time = details["allocated_hours"]
 
-            with st.container(border=True):
-                st.subheader(course_name)
+                    with st.container(border=True):
+                        st.subheader(course_name)
 
-                col1, col2 = st.columns(
-                    2,
-                    gap="medium",
-                )
+                        col1, col2 = st.columns(
+                            2,
+                            gap="medium",
+                        )
 
-                with col1:
-                    st.metric(
-                        "Difficulty",
-                        f"{difficulty}/5",
+                        with col1:
+                            st.metric(
+                                "Difficulty",
+                                f"{difficulty}/5",
+                            )
+
+                        with col2:
+                            st.metric(
+                                "Recommended Time",
+                                f"{recommended_time:.2f} h",
+                            )
+
+                else:
+                    st.info(
+                        "No study recommendations "
+                        "could be generated."
                     )
-
-                with col2:
-                    st.metric(
-                        "Recommended Time",
-                        f"{recommended_time:.2f} h",
-                    )
-
-else:
-        st.info(
-            "No study recommendations "
-            "could be generated."
-        )
 
 
 
